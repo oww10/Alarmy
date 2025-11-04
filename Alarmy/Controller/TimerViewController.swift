ad2e5ae87f13df843771e5372971e7777dcb8232
oww10 <dntjr2209@gmail.com>
2025-11-04 14:41:56 +0900

    Revert "[feat]#12 pickerView 데이터 저장하기"
    This reverts commit 75814f4d8bc35bcc51fb06c29971ee685ab68397.

--- Alarmy/Controller/TimerViewController.swift
+++ Alarmy/Controller/TimerViewController.swift
@@ -4,35 +4,82 @@ import SnapKit
 
 
 final class TimerViewController: UIViewController{
-
-    private let timerView = TimerView()
-    private var timerModel = TimerModel()
-
+    
+    private let dataPickerView: UIPickerView = {
+        let pickerView = UIPickerView()
+        return pickerView
+    }()
+    
+    private let hours = Array(0...23)
+    private let minutes = Array(0...59)
+    private let seconds = Array(0...59)
+    
+    private let timerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "타이머"
+        label.font = .systemFont(ofSize: 44, weight: .heavy)
+        label.textColor = .textColor
+        return label
+    }()
+    
+    private let cancelButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("취소", for: .normal)
+        button.backgroundColor = .cancelBGColor
+        button.setTitleColor(.cancelTextColor, for: .normal)
+        button.layer.cornerRadius = 45
+        return button
+    }()
+    
+    private let startButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("시작", for: .normal)
+        button.backgroundColor = .startBGColor
+        button.setTitleColor(.startTextColor, for: .normal)
+        button.layer.cornerRadius = 45
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view = timerView
-        settingPickerView()
-        settingAddTarget()
+        view.backgroundColor = .bgColor
+        dataPickerView.delegate = self
+        dataPickerView.dataSource = self
+        setupUI()
+        cofigureLayout()
+    }
+    
+    private func setupUI(){
+        [timerLabel, dataPickerView,cancelButton,startButton].forEach{
+            view.addSubview($0)
+        }
         
     }
-    private func settingPickerView(){
-        timerView.pickerView.delegate = self
-        timerView.pickerView.dataSource = self
-    }
-    private func settingAddTarget(){
-        timerView.startButton.addTarget(self, action: #selector(tappedStartButton), for: .touchUpInside)
-        timerView.cancelButton.addTarget(self, action: #selector(tappedCancelButton), for: .touchUpInside)
-    }
-    @objc private func tappedStartButton(){
-        
-    }
-    @objc private func tappedCancelButton(){
-        timerModel.updateTimer(hour: 0, minute: 0, second: 0)
+    private func cofigureLayout(){
+        timerLabel.snp.makeConstraints{ make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.leading.equalToSuperview().inset(20)
+        }
+        dataPickerView.snp.makeConstraints{ make in
+            make.top.equalTo(timerLabel.snp.bottom).offset(50)
+            make.centerX.equalToSuperview()
+        }
+        cancelButton.snp.makeConstraints{ make in
+            make.top.equalTo(dataPickerView.snp.bottom).offset(50)
+            make.leading.equalToSuperview().inset(20)
+            make.width.height.equalTo(90)
+        }
+        startButton.snp.makeConstraints{ make in
+            make.top.equalTo(cancelButton)
+            make.trailing.equalToSuperview().inset(20)
+            make.width.height.equalTo(90)
+        }
     }
 
 }
 
-extension TimerViewController: UIPickerViewDataSource{
+extension TimerViewController: UIPickerViewDataSource, UIPickerViewDelegate{
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 3
     }
@@ -40,11 +87,11 @@ extension TimerViewController: UIPickerViewDataSource{
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         switch component {
         case 0:
-            return timerModel.hours.count
+            return hours.count
         case 1:
-            return timerModel.minutes.count
+            return minutes.count
         case 2:
-            return timerModel.seconds.count
+            return seconds.count
         default:
             return 0
         }
@@ -54,26 +101,14 @@ extension TimerViewController: UIPickerViewDataSource{
         let title: String
         switch component {
         case 0:
-            title = "\(timerModel.hours[row]) 시"
+            title = "\(hours[row]) 시"
         case 1:
-            title = "\(timerModel.minutes[row]) 분"
+            title = "\(minutes[row]) 분"
         case 2:
-            title = "\(timerModel.seconds[row]) 초"
+            title = "\(seconds[row]) 초"
         default:
             title = ""
         }
         return NSAttributedString(string: title, attributes: [.foregroundColor: UIColor.textColor])
-    }
-    
-    
-}
-
-extension TimerViewController: UIPickerViewDelegate{
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let hoursNums = pickerView.selectedRow(inComponent: 0)
-        let minutesNums = pickerView.selectedRow(inComponent: 1)
-        let secondsNums = pickerView.selectedRow(inComponent: 2)
-    
-        timerModel.updateTimer(hour: hoursNums, minute: minutesNums, second: secondsNums)
     }
 }
